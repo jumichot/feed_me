70ad5d611d4c852e0e3fb90fddc436935627ec05
Julien Michot <julien@captive.fr>
2013-12-30 21:01:22 +0100

    Génère l'application rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FeedMe::Application.config.secret_key_base = 'c1a58083c000d81b1c8fc465d4948706415ecbf67ecdec5a9ab58a54f0a11afe5958acde7026281e745d3b12aa151b2f6f85bea9c9222fa4a78bfffbf8895626'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FeedMe::Application.config.session_store :cookie_store, key: '_feed_me_session'
