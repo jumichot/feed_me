96617ac4e05780ab8ec88b36c22cc8daa508a8e6
Julien Michot <julien@captive.fr>
2014-01-26 14:34:37 +0100

    Ajoute spe pour les le model user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,24 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#find_for_pocket_oauth" do
+    it "create user with the rights credentials" do
+      omniauth_hash = stub(
+                        :provider => "pocket",
+                        :uid => "ju",
+                        :extra => stub(:raw_info => stub(:username => 'ju_user')),
+                        :credentials => stub(:token => 'abc123'),
+                        :slice => {:provider => "pocket", :uid => "ju"}
+                      )
+
+        expect {
+          User.find_for_pocket_oauth(omniauth_hash)
+        }.to change{ User.count }.by(1)
+
+        expect(User.first.pocket_username).to eq('ju_user')
+        expect(User.first.pocket_code).to eq('abc123')
+        expect(User.first.provider).to eq('pocket')
+        expect(User.first.uid).to eq('ju')
+    end
+  end
 end
