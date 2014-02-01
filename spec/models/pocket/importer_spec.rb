f5e117f1d51ffaf6610d5508cb2c4febf46eb0b7
Julien Michot <julien@captive.fr>
2014-02-02 00:42:14 +0100

    Réfactore importer spec

--- spec/models/pocket/importer_spec.rb
+++ spec/models/pocket/importer_spec.rb
@@ -6,7 +6,17 @@ describe Pocket::Importer do
     @client = Pocket::Importer.new(build :user)
   end
 
-  describe "#ressources" do
+  def create_ressources(tags)
+    tags.inject([]) do |ressources, tag|
+      ressources << create_ressource(tag)
+    end
+  end
+
+  def create_ressource tag
+    create(:pocket_ressource,"tags" => {tag => {"item_id" => "522271957","tag" => "ruby"}})
+  end
+
+  describe "#ressources create array of ressources" do
     it "convert hash from pocket api to array of OpenStruct" do
       VCR.use_cassette 'retrieve_complete' do
         ressources =  @client.ressources
@@ -18,55 +28,25 @@ describe Pocket::Importer do
   describe "#import_topics" do
     it "import only topics that start with # with no duplicates" do
       VCR.use_cassette 'retrieve_complete' do
-        ressources = []
-        ressources << create_ressource("#ruby")
-        ressources << create_ressource("#ruby")
-        ressources << create_ressource("useless")
-        ressources << create_ressource("useless")
-        ressources << create_ressource("#tool")
+        ressources = create_ressources ["#ruby","#ruby","useless","useless","#tool"]
         @client.stubs(:ressources).returns(ressources)
 
         @client.import_topics
+
         expect(Topic.all.map{|t| t.name}).to eq ["#ruby","#tool"]
       end
     end
 
     it "create relationship with ressources" do
       VCR.use_cassette 'retrieve_complete' do
-        ressources = []
-        ressources << create_ressource("#ruby")
-        ressources << create_ressource("#ruby")
-        ressources << create_ressource("ruby")
+        ressources = create_ressources ["#ruby","#ruby","ruby"]
         @client.stubs(:ressources).returns(ressources)
 
         @client.import_topics
+
         expect(Topic.first.ressources.count).to eq 2
+        expect(Ressource.count).to eq 3
       end
     end
-  end
-
-
-  describe "#import_ressources" do
-    it "can import one ressource if resolved_id unique" do
-      VCR.use_cassette 'retrieve_complete' do
-        res1 = create(:pocket_ressource)
-        res2 = create(:pocket_ressource, :resolved_id => 252)
-        res3 = create(:pocket_ressource, :resolved_id => 252)
-        ressources = [res1, res2, res3]
-
-        @client.stubs(:ressources).returns(ressources)
-        ressources.each do |ressource|
-          @client.create_ressource ressource
-        end
-
-        expect(Ressource.count).to eq 2
-        expect(Ressource.first.resolved_id).to eq res1.resolved_id.to_i
-        expect(Ressource.last.resolved_id).to eq res2.resolved_id.to_i
-      end
-    end
-  end
-
-  def create_ressource tag
-    create(:pocket_ressource,"tags" => {tag => {"item_id" => "522271957","tag" => "ruby"}})
   end
 end
